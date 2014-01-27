cc4fd8b59617871be6bb15454123a0907451b2c1
Arnaldo Capo <hello@arnaldocapo.com>
2014-01-27 01:53:39 -0500

    Added first post request to wp-node

--- wp-node-wordpress.php
+++ wp-node-wordpress.php
@@ -8,4 +8,19 @@ Author: Arnaldo Capo
 Author URI: http://www.arnaldocapo.com/
 */
 
+
+
+add_action( 'save_post', function( $post_id ) {
+
+  // // If this is just a revision, don't send the email.
+  // if ( wp_is_post_revision( $post_id ) )
+  //  return;
+
+  $args = array();
+  $result = wp_remote_post( 'http://localhost:3000/wp-node-cache/' . $post_id, $args );
+
+});
+
+
+
 ?>
