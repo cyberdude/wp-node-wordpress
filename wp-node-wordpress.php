0d40266a103eb3a11029e744ba45056189116416
Arnaldo Capo <hello@arnaldocapo.com>
2014-01-26 21:31:10 -0500

    Initial Commit

---
+++ wp-node-wordpress.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: wp-node-wordpress
+Plugin URI: http://localhost:3000.
+Description: Provides signal communications between your Node.JS environment and WordPress
+Version: 1
+Author: Arnaldo Capo
+Author URI: http://www.arnaldocapo.com/
+*/
+
+?>
