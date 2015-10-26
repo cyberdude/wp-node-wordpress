c0858c5f779e83b98e4370f31087e1fadbd7ed56
Arnaldo Capo <arnaldo.capo@gmail.com>
2015-10-26 00:30:01 -0400

    Now categories are cleared when the user updates it

--- wp-node-wordpress.php
+++ wp-node-wordpress.php
@@ -8,6 +8,15 @@ Author: Arnaldo Capo
 Author URI: http://www.arnaldocapo.com/
 */
 
+function wp_node__send_post($args) {
+
+  $result = wp_remote_post( WP_NODE_CLEAR_CACHE_ENDPOINT, $args );
+
+  if ( is_wp_error($result) )
+    error_log( $result->get_error_message() );
+
+}
+
 add_action( 'save_post', function( $post_id ) {
 
   // Let's ignore only if this is a revision
@@ -15,15 +24,32 @@ add_action( 'save_post', function( $post_id ) {
    return;
 
   $args = array(
-    'body'  => array(
-      'secret' => WP_NODE_SECRET,
-      'post_id' => $post_id
+    'body' => array(
+      'secret'  => WP_NODE_SECRET,
+      'post_id' => $post_id,
+      'type'    => 'post'
     ),
   );
-  $result = wp_remote_post( WP_NODE_CLEAR_CACHE_ENDPOINT, $args );
 
-  if ( is_wp_error($result) )
-    error_log( $result->get_error_message() );
+  wp_node__send_post( $args );
+
+});
+
+add_action( 'edit_category', function($category_id){
+
+  $categories = split( ';', get_category_parents( $category_id, false, ';' ) );
+
+  array_pop( $categories );
+
+  $args = array(
+    'body' => array(
+      'secret'      => WP_NODE_SECRET,
+      'type'        => 'categories',
+      'categories'  => $categories
+    ),
+  );
+  
+  wp_node__send_post( $args );
 
 });
 
